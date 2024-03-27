46e2c7b72a457fa2d692c2833a5938e6d7349883
rouzbeh1384 <r.84.asgari@gmail.com>
2024-03-27 23:36:37 +0330

    Create Account TODO Hash passWord

--- redditeProject/src/main/java/org/example/Account.java
+++ redditeProject/src/main/java/org/example/Account.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.UUID;
+
 public class Account {
+
+    private String Email;
+
+    private String passWord;
+
+    private  final String Time= String.valueOf(LocalDate.now());;
+
+    private UUID uuid= UUID.randomUUID();
+
+
+   public boolean verifyPassWord(String passWord) {
+
+
+       return false;
+   }
+
+   public void changepassword(String LastpassWord,String passWord) {
+       if(verifyPassWord(LastpassWord)==true)
+       {
+           this.passWord=passWord;
+       }
+   }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+
+
+
+
 }
