3e37c6c2dfcf9c35a9ac93fd4bfcc01612aa5b73
rouzbeh1384 <r.84.asgari@gmail.com>
2024-03-28 09:17:32 +0330

    create SubReddit

---
+++ redditeProject/src/main/java/org/example/SubReddit.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.File;
+
+public class SubReddit {
+
+
+
+    public SubReddit(String Name1)
+    {
+        File  Name= new File("C:\\Users\\Asus\\IdeaProjects\\redditeProject\\src\\main\\resources");
+    }
+
+
+}
