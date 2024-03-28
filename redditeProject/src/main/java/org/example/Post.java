e163d2ad243e1ec792081516cc10216ad2f5bdf5
rouzbeh1384 <r.84.asgari@gmail.com>
2024-03-28 09:16:01 +0330

    create a post like and dislike and get name

---
+++ redditeProject/src/main/java/org/example/Post.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Post {
+
+    private String Name;
+
+    private int Like;
+
+
+    public Post(){
+
+
+    }
+
+    public String getName()
+    {
+        return this.Name;
+    }
+
+    public void setLike(int i)
+    {
+        this.Like+=i;
+    }
+
+
+
+}
