ac087d85c28fc35db0eadd85c0177493b27ad65a
rouzbeh1384 <r.84.asgari@gmail.com>
2024-03-28 09:17:17 +0330

    update create a post and like it or dislike calculus the tim of usage

--- redditeProject/src/main/java/org/example/User.java
+++ redditeProject/src/main/java/org/example/User.java
@@ -1,4 +1,51 @@
 package org.example;
 
-public class User {
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class User extends  Account{
+    public ArrayList<Post>ownPost;
+
+    public ArrayList<SubReddit> ownSubreddit;
+
+    public String userName;
+
+
+    public User(String User,String PassWord,String Email) {
+        super(PassWord,Email);
+        this.userName=User;
+        ownSubreddit=new ArrayList<>();
+        ownPost=new ArrayList<>();
+    }
+
+    public void SetPost(Post post) {
+    this.ownPost.add(post);
+    }
+    public void Like(Post post)
+    {
+        int coneter =0;
+        for(Post x:ownPost)
+        {
+            System.out.println(coneter++ +" "+x.getName());
+        }
+        Scanner scanner=new Scanner(System.in);
+        int number =scanner.nextInt();
+        System.out.println("Like or lis like Y / N ");
+        if(scanner.next().equals("Y"))
+            this.ownPost.get(number).setLike(1);
+        else
+            this.ownPost.get(number).setLike(-1);
+    }
+
+
+    public void getHour() {
+        LocalTime currentTime = LocalTime.now();
+        int HourTime= currentTime.getHour();
+        if (super.getHourTime() - HourTime < 1){
+            System.out.println("You free time is ended   ");
+            System.out.println("pay for start premium  ");
+
+        }
+    }
 }
