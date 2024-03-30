322ffdccd88b2f49d10a44539fdf2a2ff6c79a7e
rouzbeh1384 <r.84.asgari@gmail.com>
2024-03-30 14:03:12 +0330

    update

--- redditeProject/src/main/java/org/example/Main.java
+++ redditeProject/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalTime;
@@ -12,43 +13,63 @@ public class Main {
 
 
 
-    public static void main(String[] args)  {
-        ArrayList<User>users=new ArrayList<>();
-        ArrayList<Post>posts=new ArrayList<>();
-        ArrayList<SubReddit>subReddits=new ArrayList<>();
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Post> posts = new ArrayList<>();
+        ArrayList<SubReddit> subReddits = new ArrayList<>();
 
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("hi your welcome to rouzddit");
 
         while (true) {
-           System.out.print("enter your email : ");
-           Scanner scanner=new Scanner(System.in);
-           String email=scanner.next();
-           System.out.print("enter your password");
-           String passWord=scanner.next();
-           if (!users.isEmpty()){
-               for (User x:users)
-               {
-                   if(x.verifyPassWord(passWord)&x.getEmail().equals(email)){
+
+
+            System.out.print("1-create Account 2-enter ");
+            int a = scanner.nextInt();
+            System.out.print("enter yor email :");
+            String emial = scanner.next();
+            System.out.print("enter your pass word ");
+            String pass = scanner.next();
+            if (a == 2) {
+                System.out.print("enter Username");
+                User user = new User(scanner.next(), pass, emial);
+                users.add(user);
+            } else {
+                if (users.isEmpty()) {System.out.print("No account ");}
+                else {
+                    for (User x:users) {
+                        if(x.verifyPassWord(pass)&x.getEmail().equals(emial))
                         runuser(x,subReddits);
-                       break;
-                   }
+                    }
 
-               }
 
-           }
+
+                }
+
+
+
+            }
+
+
+
 
 
 
 
         }
-
-
     }
+
+
+
+
+
+
+
     public static void   runuser(User x,ArrayList<SubReddit> Sub)
     {
             System.out.println("hi "+x.userName);
-            System.out.println("1-join to subreddit 2- creat sub 3-Show my subreddit 21-exit");
+            System.out.println("1-join to subreddit 2- creat sub 3-Show my subreddit 4-21-exit");
             Scanner scanner=new Scanner(System.in);
              int i=0;
             switch (scanner.nextInt()){
--- redditeProject/src/main/java/org/example/SubReddit.java
+++ redditeProject/src/main/java/org/example/SubReddit.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SubReddit extends Account{
 
@@ -11,6 +12,7 @@ public class SubReddit extends Account{
 
     ArrayList<User> users;
 
+    ArrayList<User> Owner_Addmin;
     String Name;
     public SubReddit(String NameSub,User owner,String password) throws IOException {
         super(password);
@@ -19,10 +21,26 @@ public class SubReddit extends Account{
         posts=new ArrayList<>();
         FileWriter Name= new FileWriter("file.txt");
         Name.write("mt name is rouzbeh");
+        Owner_Addmin=new ArrayList<>();
     }
     public void joind (User x){
         users.add(x);
     }
+
+    public void setOwner_Addmin(){
+        System.out.print("enter password :");
+        Scanner scanner=new Scanner(System.in);
+        if(verifyPassWord(scanner.next())){
+            System.out.print("enter email of account ");
+            for (User x:users){
+                if (x.getEmail().equals(scanner.next())){
+                    Owner_Addmin.add(x);
+                    break;
+                }
+            }
+        }
+    }
+
 
 
 
