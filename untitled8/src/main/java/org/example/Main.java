0a57fb336a7042c68ec9112418212409fe6fd886
rouzbeh1384 <r.84.asgari@gmail.com>
2024-04-12 06:35:26 +0330

    add post  finsh

--- untitled8/src/main/java/org/example/Main.java
+++ untitled8/src/main/java/org/example/Main.java
@@ -135,9 +135,6 @@ public class Main {
                    }break;
                    case 2:
                    {
-                       boolean find=false;
-
-
                            String query="select * from subreddit";
                            ResultSet result=state.executeQuery(query);
                            System.out.println("inter Subreddit");
@@ -146,17 +143,100 @@ public class Main {
                            while (result.next() ){
                                if (result.getString(1).equals(Subname)){
                                    String username=result.getString(2);
-                                   if(!username.contains(name))
-                                       username=username+" ; "+name;
+                                   if(!username.contains(name)) {
+                                       username = username + " ; " + name;
+                                       query="update user set name='%s', username='%s',admin='%s',Time=%s  where name='%s'";
+                                       query=String.format(query,result.getString(1),username,result.getString(3),result.getString(4));
+                                       state.execute(query);
+                                       state.close();
+                                       connect.close();
+                                   }
                                    else System.out.println("you have been in subreddit ");
-
-                                   find=true;
-
                                }
                            }
 
 
-                   }
+                   }break;
+                   case 3:
+                   {
+                       int conter=0;
+                       String query="select * from subreddit";
+                       ResultSet result=state.executeQuery(query);
+                       while (result.next()){
+                           conter++;
+                           System.out.println(conter +" - "+result.getString(1)+"|_____|"+result.getString(4));
+                       }
+                       System.out.println("inter name of subreddit  or \n\"exit\"");
+                       String Sub=scanner.next();
+                       while (true) {
+                       if (Sub.equals("exit"))
+                            break;
+
+                           query = "select * from post";
+                           result = state.executeQuery(query);
+                           conter = 0;
+                            //1 Title	2 test	3 subReddit	4 writer	5 Time	6 like_	 7 dis_like
+                           while (result.next()) {
+                               if (result.getString(3).equals(Sub)) {
+                                   conter++;
+                                   System.out.println(
+                                           conter + "- Title :" + result.getString(1) + ":\n" + "post: " + result.getString(2) + "\n" +
+                                                   "Writer : " + result.getString(4) + "|Time|" + result.getString(5) +
+                                                   "| Like :|" + result.getString(6) + "|Dis_like|" + result.getString(7)
+                                   );
+
+                               }
+                           }
+                           System.out.println("1-add post \n2-add comment  \n3- show comment 4-change post(own) 21-exit ");
+                           switch (scanner.nextInt()) {
+                               case 1: {
+                                   String title = "", text = "";
+                                   boolean b=true,w=false;
+
+                                   while (b) {
+                                        query="select * from post";
+                                        result=state.executeQuery(query);
+                                        System.out.println("inter your title ");
+                                        title=scanner.next();
+
+                                       while (result.next() ){
+                                           if (result.getString(1).equals(title)){
+                                               w=true;
+                                               break;
+                                           }
+                                       }
+                                       if(w==false) {
+                                           b = false;
+                                           System.out.println("inter your post (for end enter ~)");
+                                           String str="";
+                                           while (str.equals("~")){
+                                               text=text+str;
+                                               str=scanner.next();
+                                           }
+                                       }
+                                   }
+
+                                   LocalTime localTime = LocalTime.now();
+                                   query = "insert into post (Title,test,subReddit,writer,Time,like, dis_like) values ('%s','%s','%s','%s',%s,%s,%s)";
+                                   query = String.format(title, text, name, localTime.getHour(), 0, 0);
+                                   state.execute(query);
+                                   System.out.println("Successful");
+
+
+                               }break;
+                               case 2: {
+                               }break;
+                               case 3:{
+
+                               }break;
+                               case 21:{
+                                   Sub="exit";
+                               }
+
+                           }
+
+                       }
+                   }break;
 
                }
             }
